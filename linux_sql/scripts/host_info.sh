0c4f8ebe55b99a775c4c7f92ccef035c80fc9f4f
Santhosh Srinivasagan <santhosh.srinivasagan@gmail.com>
2022-05-06 17:50:17 +0000

    harware retreival commands added

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,37 @@
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+if [ $# -ne  5 ]; then
+  echo "Insufficient arguments, please follow the format"
+  echo "host_info.sh <psql_host> <psql_port> <db_name> <psql_user> <psql_password>"
+  exit 1
+fi
+
+
+hostname=$(hostname -f)
+lscpu_out=$(lscpu)
+
+#Retrieve hardware specification variables
+cpu_number=$(echo "$lscpu_out" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out" | egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out" | egrep "^Model:" | awk '{print $2}' | xargs)
+cpu_mhz=$(echo "$lscpu_out" | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$lscpu_out" | egrep "^L2 cache:" | awk '{print $3}' | xargs)
+totalMem=$(cat /proc/meminfo | egrep "^MemTotal:" | awk '{print $2}' | xargs)
+timestamp=$(date +%Y-%m-%d" "%H:%M:%S)
+
+#Subquery to find matching id in host_info table
+host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
+
+#PSQL command: Inserts server usage data into host_usage table
+#Note: be careful with double and single quotes
+insert_stmt="INSERT INTO host_usage(timestamp, ...) VALUES('$timestamp', ..."
+
+#set up env var for pql cmd
+export PGPASSWORD=$psql_password
+#Insert date into a database
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+exit $?
